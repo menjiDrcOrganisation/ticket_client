d4aecaebaefc8b82f78781184e601a3146857b9e
marien <mmanila322@gmail.com>
2026-03-30 23:31:16 +0200

    version test

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -13,13 +13,12 @@ class ApiController extends Controller
     'verify' => false,
 ])->get(env('ENV_POINT_URL') . "/api/evenements");
 
-        // if ($response->successful()) {
-        //     $data = $response->json();
-        //     return view('evenements', ['evenements' => $data['data']]);
-        // } else {
-        //     return view('evenements', ['evenements' => [], 'error' => 'Impossible de récupérer les données']);
-        // }
-        return view('maintenance.maintenance');
+        if ($response->successful()) {
+            $data = $response->json();
+            return view('evenements', ['evenements' => $data['data']]);
+        } else {
+            return view('evenements', ['evenements' => [], 'error' => 'Impossible de récupérer les données']);
+        }
     }
 
     // Récupérer un événement spécifique via short_url
@@ -29,20 +28,18 @@ class ApiController extends Controller
     'verify' => false,
 ])->get(env('ENV_POINT_URL') . "/api/evenements/{$short_url}");
 
-        // if ($response->successful()) {
-        //     $data = $response->json();
+        if ($response->successful()) {
+            $data = $response->json();
 
-        //     if ($data['success']) {
-        //         $evenement = $data['data'];
-        //         return view('evenementsiggle', compact('evenement'));
-        //     } else {
-        //         return view('evenementsiggle', ['error' => 'Événement non trouvé']);
-        //     }
-        // } else {
-        //     return view('evenementsiggle', ['error' => 'Erreur lors de la récupération des données']);
-        // }
-
-        return view('maintenance.maintenance');
+            if ($data['success']) {
+                $evenement = $data['data'];
+                return view('evenementsiggle', compact('evenement'));
+            } else {
+                return view('evenementsiggle', ['error' => 'Événement non trouvé']);
+            }
+        } else {
+            return view('evenementsiggle', ['error' => 'Erreur lors de la récupération des données']);
+        }
     }
 
 public function sendDemandeEvenement(Request $request)
@@ -72,13 +69,11 @@ public function sendDemandeEvenement(Request $request)
             'statut' => $validated['statut'],
         ]);
 
-        // if ($response->successful()) {
-        //     return redirect()->back()->with('success', 'Demande envoyée avec succès, vous serez contacté dans les plus brefs délais');
-        // } else {
-        //     return redirect()->back()->with('error', 'Erreur lors de l\'envoi : ');
-        // }
-
-        return view('maintenance.maintenance');
+        if ($response->successful()) {
+            return redirect()->back()->with('success', 'Demande envoyée avec succès, vous serez contacté dans les plus brefs délais');
+        } else {
+            return redirect()->back()->with('error', 'Erreur lors de l\'envoi : ');
+        }
 
     } catch (\Exception $e) {
         return redirect()->back()->with('error', 'Exception rencontrée : ');
@@ -86,8 +81,7 @@ public function sendDemandeEvenement(Request $request)
 }
     public function createDemandeEvenement(Request $request)
     {
-        // return view('demandeEvenement.create');
-        return view('maintenance.maintenance');
+        return view('demandeEvenement.create');
     }
 
     }
