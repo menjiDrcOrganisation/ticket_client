0729ddead1d536d1790b68307ca7084d351c5cb1
marien <mmanila322@gmail.com>
2026-04-17 18:06:19 +0200

    style(billet): changement style billet

--- routes/web.php
+++ routes/web.php
@@ -4,24 +4,24 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiController;
 use App\Http\Controllers\PdfController;
 
-Route::get('/', function () {
-    return view('maintenance.maintenance');
-});
+// Route::get('/', function () {
+//     return view('maintenance.maintenance');
+// });
 
-// Route::get('/ticket', function () {
-//     return view('ticket');
-// })->name('accueil');
+Route::get('/ticket', function () {
+    return view('ticket');
+})->name('accueil');
 
-// Route::get('/', [ApiController::class, 'getEvenements'])->name('evenements.all');
+Route::get('/', [ApiController::class, 'getEvenements'])->name('evenements.all');
 
-// Route::get('/{short_url}', [ApiController::class, 'getEvenement'])->name('evenements.single');
+Route::get('/{short_url}', [ApiController::class, 'getEvenement'])->name('evenements.single');
 
-// Route::get('/ticket/pdf', [PdfController::class, 'generateTicket'])->name('ticket.pdf');
+Route::get('/ticket/pdf', [PdfController::class, 'generateTicket'])->name('ticket.pdf');
 
-// Route::post('/ticket/generate-pdf', [PdfController::class, 'generateTicket'])
-//     ->name('ticket.generate.pdf');
+Route::post('/ticket/generate-pdf', [PdfController::class, 'generateTicket'])
+    ->name('ticket.generate.pdf');
 
-// Route::get('/ticket/test', [PdfController::class, 'testTicket']);
+Route::get('/ticket/test', [PdfController::class, 'testTicket']);
 
-// Route::get('/demandeEvenement/create', [ApiController::class, 'createDemandeEvenement'])->name('demandeEvenement.create');
-// Route::post('/demandeEvenement/send', [ApiController::class, 'sendDemandeEvenement'])->name('demandeEvenement.send');
+Route::get('/demandeEvenement/create', [ApiController::class, 'createDemandeEvenement'])->name('demandeEvenement.create');
+Route::post('/demandeEvenement/send', [ApiController::class, 'sendDemandeEvenement'])->name('demandeEvenement.send');
